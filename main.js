d66dc5fab8028a61b00528602daa56dd32790acf
shunta <exec.apea@gmail.com>
2019-08-24 12:58:53 +0900

    add

---
+++ main.js
@@ -0,0 +1,37 @@
+const app = new Vue({
+  el: "#app",
+
+  data: {
+    bpi: null,
+    hasError: false,
+    loading: true
+  },
+
+  mounted: function() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(
+        function(response) {
+          console.log(response.data.bpi);
+          this.bpi = response.data.bpi;
+        }.bind(this)
+      )
+      .catch(
+        function(error) {
+          console.log(error);
+          this.hasError = true;
+        }.bind(this)
+      )
+      .finally(
+        function() {
+          this.loading = false;
+        }.bind(this)
+      );
+  },
+
+  filters: {
+    currencyDecimal(value) {
+      return value.toFixed(2);
+    }
+  }
+});
